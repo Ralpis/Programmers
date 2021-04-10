edd0aded90f28c9c39f7dc5058a4aa33a7a78c7a
Ralpis <rotjsdn1@naver.com>
2021-04-10 17:22:35 +0900

    43162 네트워크 주석추가

--- 43162.py
+++ 43162.py
@@ -19,12 +19,16 @@ n	computers	return
 3	[[1, 1, 0], [1, 1, 1], [0, 1, 1]]	1
 """
 
+# DFS로 노드 찾아 떠나자
+
 
 def dfs(graph, v, visited):
     visited[v] = True
     n = len(graph)
 
     for node in range(n):
+        # 그래프에서 라인 있는 곳과 방문한 적이 없다면 네트워크라인이므로 이어진 부분으로 간주해서 찾는 다.
+        # 아니라면 이미 지나갔거나 없는 경우다
         if graph[v][node] == 1 and visited[node] == False:
             dfs(graph, node, visited)
 
